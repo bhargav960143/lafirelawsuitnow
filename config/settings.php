eb11c7d84a6d035fd0b289146b7e964f07f2b849
Bhargav Patel <bhargav960143@gmail.com>
2025-02-26 14:30:30 +0530

    update email

--- config/settings.php
+++ config/settings.php
@@ -2,7 +2,7 @@
 
 return[
     'sheet_id' => '1vb4a0iIQSORd2ImjoVopW3ksbyQc0Z4wsPMSrTxEXPo',
-    'to_email' => 'jay@movingnode.com',
+    'to_email' => 'msconsultingleads@gmail.com',
     'captcha_site_key' => '6Le89uIqAAAAAEwyhZfMdUrPDZksoHCntafkIzc3',
     'captcha_secret_key' => '6Le89uIqAAAAAE7ZrQaUcBhXCMlovU4k04WuRshi',
 ];
